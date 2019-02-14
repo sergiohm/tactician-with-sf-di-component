9c1d8eae1d899b3eb6924eca963bdd32d30718d8
Sergio Hernández Martínez <hm.sergio@gmail.com>
2019-02-14 22:14:45 +0100

    Refactor name extractor suffix like parameter

--- src/Infrastructure/Bus/CommandNameExtractor/ClassWithSuffix.php
+++ src/Infrastructure/Bus/CommandNameExtractor/ClassWithSuffix.php
@@ -7,8 +7,15 @@ use League\Tactician\Handler\CommandNameExtractor\CommandNameExtractor;
 
 class ClassWithSuffix implements CommandNameExtractor
 {
+    private $suffix;
+
+    public function __construct(string $suffix)
+    {
+        $this->suffix = $suffix;
+    }
+
     public function extract($command)
     {
-        return get_class($command) . '.handler';
+        return sprintf('%s%s', get_class($command), $this->suffix);
     }
 }
